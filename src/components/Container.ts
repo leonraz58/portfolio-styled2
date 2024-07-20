916657751b221682be1aaf6a93eeee4b273462a8
leonraz58 <leonraz58@gmail.com>
2024-07-20 18:00:27 +0300

    add Container

---
+++ src/components/Container.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 1250px;
+    width: 100%;
+    min-height: 100%;
+    padding: 0 25px;
+    margin: 0 auto;
+    //outline: 1px solid red; 
+`
