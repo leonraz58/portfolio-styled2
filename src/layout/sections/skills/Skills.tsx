aa4d97994ef7e7b63dfe233d4259a106fa1a70ad
leonraz58 <leonraz58@gmail.com>
2024-07-29 21:51:00 +0300

    add adaptive to Skills

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
-import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Skill} from "./skill/Skill";
 import {Container} from "../../../components/Container";
 
+
+
 export const Skills = () => {
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const tablet = 768;
+    const mobile = 576;
+
+    const handleWindowResize = () => setWidth(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize );
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize)
+        }
+    }, []);
+
+    const calcSize = () => {
+        return width > mobile ? (width > tablet ? '70px' : '108px') : '60px';
+    }
+
     return (
         <StyledSkills>
             <Container>
-                <FlexWrapper gap={"13px"} justify={"center"} align={"center"}>
-                    <Skill iconId={"ps"} width={"70px"} height={"70px"} viewBox={"3 3 38 38"} label={"Photoshop"}/>
-                    <Skill iconId={"ae"} width={"70px"} height={"70px"} viewBox={"3 3 38 38"} label={"After Effects"}/>
-                    <Skill iconId={"illustrator"} width={"70px"} height={"70px"} viewBox={"3 3 38 38"}
+                <SkillsWrapper>
+                    <Skill iconId={"ps"} width={calcSize()}
+                           height={calcSize()}
+                           viewBox={"3 3 38 38"}
+                           label={"Photoshop"}/>
+                    <Skill iconId={"ae"} width={calcSize()} height={calcSize()} viewBox={"3 3 38 38"} label={"After Effects"}/>
+                    <Skill iconId={"illustrator"} width={calcSize()} height={calcSize()} viewBox={"3 3 38 38"}
                            label={"Illustrator"}/>
-                    <Skill iconId={"figma"} width={"70px"} height={"70px"} viewBox={"-4 -3 54 54"} label={"Figma"}/>
-                </FlexWrapper>
+                    <Skill iconId={"figma"} width={calcSize()} height={calcSize()} viewBox={"-4 -3 54 54"} label={"Figma"}/>
+                </SkillsWrapper>
             </Container>
         </StyledSkills>
     );
@@ -24,5 +47,32 @@ const StyledSkills = styled.section`
     width: 100%;
     background-image: linear-gradient(to right, #A6BCFA 30%, #FFFFFF 30%);
     padding-bottom: 90px;
+    background-color: white;
+
+    @media ${({theme}) => theme.media.tablet} {
+        background-image: none;
+        padding-top: 65px;
+    }
+    
+`
+
+const SkillsWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 161px);
+    gap: 12px;
+    justify-content: center;
+    justify-items: center;
+
+    @media ${({theme}) => theme.media.tablet} {
+        grid-template-columns: repeat(2, minmax(150px, 271px));
+        row-gap: 75px;
+        column-gap: 22px;
+    }
+
+    @media ${({theme}) => theme.media.mobile} {
+    //@media screen and (max-width: 480px) {
+        row-gap: 40px;
+        column-gap: 12px;
+    }
 `
 
--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -28,6 +28,22 @@ const StyledSkill = styled.div `
     background-color: #A6BCFA;
     text-align: center;
     box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.15);
+
+    @media ${({theme}) => theme.media.tablet} {
+        max-width: 271px;
+        width: 100%;
+        height: 153px;
+        border-radius: 40px;
+        font-size: 33px;
+    }
+
+    @media ${({theme}) => theme.media.mobile} {
+        max-width: 150px;
+        width: 100%;
+        height: 82px;
+        border-radius: 22px;
+        font-size: 18px;
+    }
 `
 
 const IconWrapper = styled.div`
@@ -38,6 +54,18 @@ const IconWrapper = styled.div`
     border: 10px solid white;
     overflow: hidden;
     background-color: #000B1D;
+
+    @media ${({theme}) => theme.media.tablet} {
+        width: 108px;
+        height: 108px;
+        border: 16px solid white
+    }
+
+    @media ${({theme}) => theme.media.mobile} {
+        width: 60px;
+        height: 60px;
+        border: 10px solid white
+    }
 `
 
 const Label = styled.span`
