0a6e39306670ebd67998ddd4cce12db404b915a2
leonraz58 <leonraz58@gmail.com>
2024-07-30 15:49:29 +0300

    add adaptive to Services

--- src/layout/sections/services/Services.tsx
+++ src/layout/sections/services/Services.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from "styled-components";
 import {Container} from "../../../components/Container";
 import {SectionTitle} from "../../../components/SectionTitle";
-import {FlexWrapper} from "../../../components/FlexWrapper";
 import {ServiceCard} from "./serviceCard/ServiceCard";
 
 export const Services = () => {
@@ -10,20 +9,46 @@ export const Services = () => {
         <StyledServices>
             <Container>
                 <SectionTitle>Services</SectionTitle>
-                <FlexWrapper justify={"center"} gap={"31px"}>
+                <Cards>
                     <ServiceCard text={"Video Edit Service"} iconId={"videoPlay"}/>
-                    <ServiceCard text={"Ui/Ux Designer"} iconId={"edit"}/>
+                    <ServiceCard text={"Ui/Ux \nDesigner"} iconId={"edit"}/>
                     <ServiceCard text={"Website Developer"} iconId={"notebook"}/>
-                </FlexWrapper>
+                </Cards>
             </Container>
         </StyledServices>
     );
 };
 
 const StyledServices = styled.section`
-    margin: 65px 0 97px;
+    padding: 65px 0 97px;
     ${SectionTitle} {
         margin-bottom: 83px;
+    }
+
+    @media ${({theme}) => theme.media.mobile }{
+        ${SectionTitle} {
+            margin-bottom: 50px;
+        }
+        padding: 50px 0 55px;
+    }
+`
+
+const Cards = styled.ul`
+    display: flex;
+    justify-content: center;
+    column-gap: 31px;
+    row-gap: 80px;
+    flex-wrap: wrap;
+    
+
+    @media ${({theme}) => theme.media.tablet }{
+        column-gap: 25px;
+        row-gap: 60px;
+    }
+
+    @media ${({theme}) => theme.media.mobile }{
+        column-gap: 15px;
+        row-gap: 33px;
     }
 `
 
--- src/layout/sections/services/serviceCard/ServiceCard.tsx
+++ src/layout/sections/services/serviceCard/ServiceCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {FlexWrapper} from "../../../../components/FlexWrapper";
 import {Icon} from "../../../../components/Icon/Icon";
 import styled from "styled-components";
@@ -9,26 +9,47 @@ type ServiceCardPropsType = {
 }
 
 export const ServiceCard = (props: ServiceCardPropsType) => {
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const tablet = 768;
+    const mobile = 576;
+
+    const handleWindowResize = () => setWidth(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize );
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize)
+        }
+    }, []);
+
+    const calcSize = () => {
+        return width > mobile ? (width > tablet ? '73px' : '62px') : '35px';
+    }
+
     return (
         <StyledServiceCard>
             <FlexWrapper direction={"column"} justify={"flex-end"}>
                 <Icon
                     iconId={props.iconId}
-                    width={"73"}
-                    height={"73"}
+                    width={calcSize()}
+                    height={calcSize()}
                     viewBox={'0 0 73 73'}
                 />
                 <Text>{props.text}</Text>
 
-                <Link><FlexWrapper align={"center"} gap={"15px"}>View More
-                    <Icon
-                        iconId={"arrow"}
-                        width={"21"}
-                        height={"21"}
-                        viewBox={"0 0 21 21"}
-                        fill={"#2157F2"}
-                    />
-                </FlexWrapper>
+                <Link>
+                    <FlexWrapper align={"center"} gap={"15px"}>
+                        <span>View More</span>
+                        <Icon
+                            iconId={"arrow"}
+                            width={"21"}
+                            height={"21"}
+                            viewBox={"0 0 21 21"}
+                            fill={"#2157F2"}
+                        />
+                    </FlexWrapper>
                 </Link>
 
             </FlexWrapper>
@@ -37,12 +58,25 @@ export const ServiceCard = (props: ServiceCardPropsType) => {
 };
 
 const StyledServiceCard = styled.div`
-    width: 334px;
+    max-width: 334px;
+    width: 100%;
     height: 402px;
     color: black;
     box-shadow: 0 15px 29px 0 rgba(0, 0, 0, 0.05);
     background: #fff;
     padding: 0 59px 45px;
+
+    @media ${({theme}) => theme.media.tablet }{
+        max-width: 280px;
+        height: 338px;
+        padding: 90px 10px 38px 50px;
+    }
+
+    @media ${({theme}) => theme.media.mobile }{
+        max-width: 155px;
+        height: 187px;
+        padding: 55px 10px 20px 27px;
+    }
 `
 
 const Link = styled.a`
@@ -50,19 +84,16 @@ const Link = styled.a`
     font-size: 16px;
     line-height: 196%;
     color: #2157f2;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 15px;
-    
-    ${FlexWrapper} {
+
+    & span {
         position: relative;
-        
+
         &:hover {
             &::before {
                 width: 100%;
-            }    
+            }
         }
+
         &::before {
             content: "";
             display: inline-block;
@@ -77,6 +108,25 @@ const Link = styled.a`
             transition: .2s ease-in-out;
         }
     }
+    
+    ${FlexWrapper} {
+        
+        
+        @media ${({theme}) => theme.media.tablet }{
+            gap: 11px;
+        }
+        @media ${({theme}) => theme.media.mobile }{
+            gap: 5px;
+        }
+    }
+
+    @media ${({theme}) => theme.media.tablet }{
+        font-size: 13px;
+    }
+    @media ${({theme}) => theme.media.mobile }{
+        font-size: 7px;
+    }
+    
 `
 
 const Text = styled.span`
@@ -86,4 +136,15 @@ const Text = styled.span`
     font-weight: 600;
     line-height: 125%;
     margin: 25px 0 30px;
+
+    @media ${({theme}) => theme.media.tablet }{
+        font-size: 27px;
+        margin: 18px 0 16px;
+    }
+
+    @media ${({theme}) => theme.media.mobile }{
+        //max-width: 70px;
+        font-size: 15px;
+        margin: 8px 0 2px;
+    }
 `
