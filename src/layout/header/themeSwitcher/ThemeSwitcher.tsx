3471817df947aee5011287912bf1f02060c4326d
leonraz58 <leonraz58@gmail.com>
2024-07-31 20:34:44 +0300

    add ThemeSwitcher

---
+++ src/layout/header/themeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,10 @@
+import {Icon} from "../../../components/Icon/Icon";
+
+export const ToggleTheme = () => {
+
+    return (
+        <button>
+            <Icon iconId={"moon"} width={"36"} height={"37"} viewBox={"0 0 25 25"}/>
+        </button>
+    );
+}
